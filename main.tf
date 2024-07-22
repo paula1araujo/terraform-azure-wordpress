856b0b048cb85917c6a0f9897c2b06c7066cbbb4
paula1araujo <paula1_araujo@hotmail.com>
2024-07-22 14:07:17 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,18 +21,6 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "meuNIC"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
 resource "azurerm_public_ip" "public_ip" {
   name                = "myPublicIP"
   location            = azurerm_resource_group.rg.location
@@ -84,19 +72,24 @@ resource "azurerm_virtual_machine" "vm" {
     disable_password_authentication = false
   }
 
-  connection {
-    type     = "ssh"
-    user     = "adminuser"
-    password = "Password1234!"
-    host     = azurerm_public_ip.public_ip.ip_address
-  }
-
   provisioner "file" {
+    connection {
+      type     = "ssh"
+      user     = "adminuser"
+      password = "Password1234!"
+      host     = azurerm_public_ip.public_ip.ip_address
+    }
     source      = "install_docker.sh"
     destination = "/tmp/install_docker.sh"
   }
 
   provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "adminuser"
+      password = "Password1234!"
+      host     = azurerm_public_ip.public_ip.ip_address
+    }
     inline = [
       "chmod +x /tmp/install_docker.sh",
       "sudo /tmp/install_docker.sh"
@@ -107,3 +100,4 @@ resource "azurerm_virtual_machine" "vm" {
 output "public_ip" {
   value = azurerm_public_ip.public_ip.ip_address
 }
+
