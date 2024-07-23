75421ab923312ec627d76d3105bed45a656108a5
paula1araujo <paula1_araujo@hotmail.com>
2024-07-23 11:59:42 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,26 @@
+# output.tf
+
+# Saída para o nome da rede virtual
+output "vnet_name" {
+  value = azurerm_virtual_network.vnet.name
+}
+
+# Saída para o nome da sub-rede
+output "subnet_name" {
+  value = azurerm_subnet.subnet.name
+}
+
+# Saída para o nome do grupo de recursos
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+# Saída para o nome da máquina virtual
 output "vm_name" {
   value = azurerm_virtual_machine.vm.name
 }
 
-output "admin_username" {
-  value = var.admin_username
-}
-
-output "public_ip_address" {
-  value = azurerm_virtual_machine.vm.public_ip_address
+# Saída para o endereço IP público
+output "public_ip" {
+  value = azurerm_public_ip.public_ip.ip_address
 }
